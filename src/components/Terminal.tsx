5e241b1fea30b2379e9d876b76846799f658cb24
A. Bryant <W.Sword.09@hotmail.com>
2026-06-08 15:10:14 -0600

    refactor: terminal component variables

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -7,7 +7,7 @@ import { useCrtContext } from '@/app/context/CrtContext';
 
 const Terminal = () => {
 
-  const { tArray, language } = useLanguageContext();
+  const { t, tArray, language } = useLanguageContext();
   const { crt } = useCrtContext();
   const divRef = useRef<HTMLDivElement>(null);
   const inputRef = useRef<HTMLInputElement>(null!);
@@ -16,26 +16,20 @@ const Terminal = () => {
   const [slicedText, setSlicedText] = useState("");
   const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
 
-  const banner: string = tArray("banner").join('\n');
-  const help: string = tArray("help").join('\n');
-  const skills: string = tArray("skills").join('\n');
-  const quotes: string = tArray("quotes").join('\n');
-  const smallSkills: string = tArray("smallSkills").join('\n');
-  const smallBanner: string = tArray("smallBanner").join('\n');
-  const smallHelp: string = tArray("smallHelp").join('\n');
-  let speed = 5; // ms per character
-  if (window.innerWidth <= 760) speed = 50;
+  const smallScreen = window.innerWidth <= 760;
+  const joinArray = (key: string, sep: string) => tArray(key).join(sep);
+  const sep: string = '\n';
+  const banner: string = smallScreen ? joinArray("smallBanner", sep) : joinArray("banner", sep);
+  const help: string = smallScreen ? joinArray("smallHelp", sep) : joinArray("help", sep);
+  const skills: string = smallScreen ? joinArray("smallSkills", sep) : joinArray("skills", sep);
+  const quotes: string = joinArray("quotes", sep)
+  const speed: number = smallScreen ? 50 : 5
 
   useEffect(() => {
-    if (window.innerWidth <= 760) {
-      animateText('', smallBanner);
-    }
-    else {
-      animateText('', banner);
-    }
+    animateText('', banner, '');
   }, [language]);
 
-  const animateText = (base: string, textToAppend: string) => {
+  const animateText = (base: string, textToAppend: string, link?: string) => {
     if (timeoutRef.current) clearTimeout(timeoutRef.current);
     setOutput(base);
     let i = 0;
@@ -47,6 +41,8 @@ const Terminal = () => {
         timeoutRef.current = setTimeout(tick, speed);
       }
       else {
+        if (link != '') setTimeout(() => { window.open(link, '_blank'); }, 500);
+
         setOutput(base + textToAppend);
         setSlicedText("");
       }
@@ -63,40 +59,45 @@ const Terminal = () => {
   }, []);
 
   useEffect(() => {
-    if (divRef.current) {
-      divRef.current.scrollTop = divRef.current.scrollHeight;
-    }
+    if (divRef.current) divRef.current.scrollTop = divRef.current.scrollHeight;
+
     inputRef.current.focus()
   }, [slicedText]);
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
       let textToAppend = "";
+      let link = "";
       textToAppend = "@guest from portfolio\n" + "> " + input + "\n\n";
+      let openLink = t("opening") + " " + input + "..." + "\n\n";
       switch (input) {
         case "banner":
-          textToAppend += window.innerWidth <= 760 ? smallBanner : banner;
+          textToAppend += banner;
           break;
         case "help":
-          textToAppend += window.innerWidth <= 760 ? smallHelp : help;
+          textToAppend += help;
           break;
         case "skills":
-          textToAppend += window.innerWidth <= 760 ? smallSkills : skills;
+          textToAppend += skills;
           break;
         case "quotes":
           textToAppend += quotes + "\n\n";
           break;
         case "github":
-          window.open("https://github.com/AB10110F", '_blank');
+          textToAppend += openLink
+          link = "https://github.com/AB10110F";
           break;
         case "codepen":
-          window.open("https://codepen.io/AB10110F", '_blank');
+          textToAppend += openLink
+          link = "https://codepen.io/AB10110F";
           break;
         case "reddit":
-          window.open("https://www.reddit.com/user/AB10110F", '_blank');
+          textToAppend += openLink
+          link = "https://www.reddit.com/user/AB10110F";
           break;
         case "twitter":
-          window.open("https://twitter.com/AB10110F", '_blank');
+          textToAppend += openLink
+          link = "https://twitter.com/AB10110F";
           break;
         case "cls":
           if (timeoutRef.current) clearTimeout(timeoutRef.current);
@@ -105,10 +106,10 @@ const Terminal = () => {
           setInput("");
           break;
         default:
-          textToAppend = language === 'English' ? `x_x Syntax Error "${input}" is not a command\n\n` : `x_x Error de sintaxis "${input}" no es un comando\n\n`;
+          textToAppend = t("errPart1") + " \"" + input + "\" " + t("errPart2") + "\n\n";
       }
 
-      if (!(input == "cls")) animateText(output, textToAppend);
+      if (!(input == "cls")) animateText(output, textToAppend, link);
       setInput("")
     }
   };
@@ -116,7 +117,8 @@ const Terminal = () => {
   return (
     <div ref={divRef} className={`${styles.terminal} ${crt ? "bright__border" : ""}`} onClick={() => inputRef.current.focus()}>
       <pre style={vt323.style} className={styles.terminal__history}>
-        {output}{slicedText}
+        {output}
+        {slicedText}
       </pre>
       <section className={styles.terminal__prompt}>
         <article style={vt323.style}>@guest from portfolio</article>
